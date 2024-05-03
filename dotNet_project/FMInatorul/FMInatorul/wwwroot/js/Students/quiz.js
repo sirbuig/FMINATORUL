ce657c84f10bb2b11a6ee0c062d616fd19d5ede4
sirbuig <sirbuig@proton.me>
2024-05-03 14:15:26 +0300

    :truck: Restructuring .cshtml files into separate .css and .js files
    Separate inline JavaScript and CSS from .cshtml files into dedicated .js and .css files to improve maintainability and scalability of the codebase. Updated references in all affected views to ensure functionality remains consistent.

---
+++ dotNet_project/FMInatorul/FMInatorul/wwwroot/js/Students/quiz.js
@@ -0,0 +1,48 @@
+﻿let currentQuestion = 0;
+const questionBoxes = document.querySelectorAll(".question-box");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const submitButton = document.querySelector('input[type="submit"]');
+
+if (questionBoxes.length > 0) {
+    questionBoxes[0].style.display = 'block';
+    checkButtons();
+}
+
+function showQuestion(index) {
+    questionBoxes.forEach(box => box.style.display = "none");
+    questionBoxes[index].style.display = "block";
+}
+
+prevButton.addEventListener('click', () => {
+    if (currentQuestion > 0) {
+        currentQuestion--;
+        showQuestion(currentQuestion);
+        checkButtons();
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    if (!validAnswer()) {
+        alert("Trebuie sa selectezi un raspuns.");
+        return;
+    }
+
+    if (currentQuestion < questionBoxes.length - 1) {
+        currentQuestion++;
+        showQuestion(currentQuestion);
+        checkButtons();
+    }
+});
+
+function validAnswer() {
+    const currentBox = questionBoxes[currentQuestion];
+    const radios = currentBox.querySelectorAll('input[type="radio"]');
+    return Array.from(radios).some(radio => radio.checked);
+}
+
+function checkButtons() {
+    prevButton.disabled = currentQuestion === 0;
+    nextButton.disabled = currentQuestion === questionBoxes.length - 1;
+    submitButton.style.display = currentQuestion === questionBoxes.length - 1 ? "block" : "none";
+}
---
+++ dotNet_project/FMInatorul/FMInatorul/wwwroot/js/Students/results.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const againBtn = document.getElementById('playAgain');
+    const exitBtn = document.getElementById('exit');
+
+    againBtn.addEventListener('click', function () {
+        window.location.href = againBtn.getAttribute('data-url');
+    });
+
+    exitBtn.addEventListener('click', function () {
+        window.location.href = exitBtn.getAttribute('data-url');
+    });
+});
