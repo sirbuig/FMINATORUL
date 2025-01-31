b943f88c9ba2dae10ada6df155682b9ed0027d31
SIG <46359762+sirbuig@users.noreply.github.com>
2025-01-31 11:30:19 +0200

    Add Docker support (#29)
    * feat(docker): add Docker deployment
    
    * docs: Update README with Docker instructions
    
    * Integrate dotnet-ef tools

---
+++ dotNet_project/FMInatorul/FMInatorul/Data/Migrations/20250131084807_AddStudentMistakes.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FMInatorul.Migrations
+{
+    public partial class AddStudentMistakes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentMistakes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    IntrebareId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentMistakes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentMistakes_IntrebariRasps_IntrebareId",
+                        column: x => x.IntrebareId,
+                        principalTable: "IntrebariRasps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_StudentMistakes_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentMistakes_IntrebareId",
+                table: "StudentMistakes",
+                column: "IntrebareId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentMistakes_StudentId",
+                table: "StudentMistakes",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentMistakes");
+        }
+    }
+}
